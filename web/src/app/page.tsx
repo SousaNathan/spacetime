26e691ba51a89081c4b46b8808bce76a2ac604be
Nathan Sousa <nathan.wks@outlook.com>
2023-05-20 02:38:10 -0300

    inicio layout web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>
-      Hello World!
-    </h1>
+    <main className="grid grid-cols-2 min-h-screen">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative">
+
+      </div>
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360]">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="#" className="underline hover:text-gray-50">criar agora!</a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
